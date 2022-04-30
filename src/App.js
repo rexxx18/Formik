1dbd54ff916689de88828964ba38c25d0d3635dd
rexxx18 <kalitarup90@gmail.com>
2022-04-30 20:56:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Formik, useFormik } from "formik";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const formik=useFormik({
+    initialValues:{email:"",password:""},
+    validate:(values)=>{
+      const errors={};
+      if (!values.email) {
+        errors.title = "Required Title";
+      }
+      if (!values.password) {
+        errors.completed = "Required Completed";
+      }
+      return errors;
+    },
+    onSubmit:(values) => {
+      
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos/", {
+          body: JSON.stringify(values)
+        })
+        .then(function (response) {
+          console.log(response,"hi");
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  })
+
+  const {handleChange,handleSubmit,values,errors}=formik
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+      className="App"
+    >
+      <div>
+        <h1>TO DO </h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Email</label>
+        <input
+          type="text"
+          name="email"
+          value={values.email}
+      
+          onChange={handleChange}
+        />
+        {/* {errors.title ? errors.title : null} */}
+        <br />
+        <label htmlFor="completed">Password</label>
+        <input
+          type="text"
+          name="password"
+          value={values.password}
+          
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
